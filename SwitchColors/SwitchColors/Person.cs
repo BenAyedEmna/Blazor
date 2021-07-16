32320400a113c8caed93688530b2192f32a6c3e5
BenAyedEmna <emnabenayed.ca@gmail.com>
2021-07-16 20:03:44 +0200

    blazor tests

---
+++ SwitchColors/SwitchColors/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SwitchColors
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool ChangeColor { get; set; } 
+        public Person(string first,string last)
+        {
+            this.FirstName = first;
+            this.LastName = last;
+            this.ChangeColor = false;
+        }
+        public bool ChangeTheColor()
+        {
+            return (this.ChangeColor=true); 
+        }
+        public void Initial()
+        {
+            this.ChangeColor = false; 
+        }
+    }
+}
